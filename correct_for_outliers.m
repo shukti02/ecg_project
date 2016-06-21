aa2af396bfd3f22216872854dd7d5ed375fc1d61
Shukti <shukti.official@gmail.com>
2016-06-21 15:01:10 +0200

    "created function for outlier correction"

---
+++ correct_for_outliers.m
@@ -0,0 +1,33 @@
+function [out_mat] = correct_for_outliers(matrix_orig)
+%correcting for each parameter separately, based on relevance
+%inputs: matrix_orig: matrix containing rows as parameters(features) from MS models 
+%        and columns as instances  
+
+%outputs: out_mat: contains indices of outliers
+
+out_mat = cell(99,1);
+
+%tau cannot be larger that the max spacing between two beats
+tau_vector = [2 3 4 5 18 19 24 25 26 27];
+
+for i = 1 : length(tau_vector)
+    out_mat{tau_vector(i)} = find(abs(matrix_orig(tau_vector(i),:))>max(matrix_orig(100,:)));
+end
+
+%widths of gaussians cannot be larger than the max spacing between two
+%beats
+w_vector = [10:17,22,23,32:39];
+
+for i = 1 : length(w_vector)
+    out_mat{w_vector(i)} = find(abs(matrix_orig(w_vector(i),:))>max(matrix_orig(100,:)));
+end
+
+
+%amplitudes represent the voltage of the signal in mV. This definitely
+%cannot be higher than 100V.
+M_vector = [6:9,20,21,28:31,40:99];
+
+for i = 1 : length(M_vector)
+    out_mat{M_vector(i)} = find(abs(matrix_orig(M_vector(i),:))>10^5);
+end
+end
